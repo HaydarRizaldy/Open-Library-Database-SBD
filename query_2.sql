3efdc19c890eb41b26834e8c8bc956b3db1345f0
Haydar Rizaldy <52734498+HaydarRizaldy@users.noreply.github.com>
2021-03-14 20:35:16 +0700

    Update query_2.sql

--- query_2.sql
+++ query_2.sql
@@ -5,6 +5,6 @@ WHERE book.title = 'Maverick'
 ORDER BY return_books;
 
 -- 3 tables: Menampilkan data mahasiswa, buku dan reservasi dalam jangka waktu 1 Januari 2021 hingga saat ini serta diurutkan berdasarkan reservation_id secara ascending
-SELECT reservation_id, reservation.return_books, book.book_id, book.title, student.student_id, student.first_name, student.last_name, student.email
-FROM student, book, reservation
-WHERE (return_books BETWEEN to_date('2021-01-01 00:00:00', 'YYYY-DD-MM HH24:MI:SS') AND SYSDATE);
+select reservation_id, reservation.return_books, book.book_id, book.title, student.student_id, student.first_name, student.last_name, student.email
+from student, book, reservation
+where (return_books between to_date('2021-01-01 00:00:00', 'YYYY-DD-MM HH24:MI:SS') and sysdate) order by reservation.return_books;
